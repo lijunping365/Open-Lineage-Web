49b581026b343a8f0bf1e5bab464e173d6b109c9
lijunping <2544054976@qq.com>
2023-04-15 08:33:26 +0800

    :tada: 数据血缘初始化

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App () {
+  return (
+    <div className="App">
+      <div className='flex justify-center items-center'>
+        11111
+      </div>
+    </div>
+  )
+}
+
+export default App
