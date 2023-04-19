1969046bdde31f65223758b27336fb9143568fc2
lijunping006 <lijunping006@ke.com>
2023-04-19 17:37:09 +0800

    :sparkles: 优化及修复非字段血缘表及连线创建

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -148,215 +148,6 @@ export const initData = (count: number) => {
   return nodeArray;
 };
 
-// 自定义数据转换
-export const transformData = (data: any) => {
-  const nodes: any[] = [];
-  const edgeMap: Map<string, any> = new Map();
-  const tableFields: Set<any> = new Set();
-
-  data.forEach((item: any) => {
-    const targetField = item.targetField;
-    const tableField = handleTableField(targetField);
-    tableFields.add(tableField);
-    if (!item.refFields) {
-      return;
-    }
-    createEdge(edgeMap, tableFields, tableField, item.refFields);
-  });
-
-  const edges = Array.from(edgeMap.values());
-  createNode(nodes, tableFields);
-
-  return {
-    nodes,
-    edges,
-  };
-};
-
-const createEdge = (
-  edgeMap: Map<string, any>,
-  tableFields: Set<any>,
-  tableField: string,
-  refFields: any[]
-) => {
-  const target = getTableFieldName(tableField);
-  const targetName = target.tableName;
-  const targetAnchor = target.tableField;
-
-  refFields.forEach((ref: any) => {
-    const tableField = handleTableField(ref);
-    tableFields.add(tableField);
-
-    const source = getTableFieldName(tableField);
-    const sourceName = source.tableName;
-    const sourceAnchor = source.tableField;
-    if (targetName === sourceName) {
-      return;
-    }
-
-    const edge: any = {};
-    edge.source = sourceName;
-    edge.sourceAnchor = sourceAnchor;
-    edge.target = targetName;
-    edge.targetAnchor = targetAnchor;
-    edge.label = ref.label;
-    let key = sourceName + sourceAnchor + '-' + targetName + targetAnchor;
-    edgeMap.set(key, edge);
-  });
-};
-
-const handleTableField = (item: any) => {
-  const fieldName = item.fieldName;
-  let tableField = '';
-  if (item.final) {
-    tableField = fieldName;
-  } else {
-    tableField = `${item.level}-${item.index}:${fieldName}`;
-  }
-  return tableField;
-};
-
-const getTableFieldName = (item: string) => {
-  const names: string[] = item.split(':');
-  let tableName = '';
-  let tableField = '';
-  if (names.length === 1) {
-    const array = names[0].split('.');
-    tableName = array[1];
-    tableField = array[2];
-  } else {
-    const array = names[1].split('.');
-    tableName = array[1] + '_' + names[0];
-    tableField = array[2];
-  }
-  return { tableName, tableField };
-};
-
-const createNode = (nodes: any[], tableFields: Set<any>) => {
-  const tables: Map<string, string[]> = new Map();
-  tableFields.forEach((item: any) => {
-    const table = getTableFieldName(item);
-    const tableName = table.tableName;
-    const tableField = table.tableField;
-
-    if (!tables.has(tableName)) {
-      tables.set(tableName, [tableField]);
-    } else {
-      const attrs: any = tables.get(tableName);
-      if (!attrs?.includes(tableField)) {
-        attrs?.push(tableField);
-        tables.set(tableName, attrs);
-      }
-    }
-  });
-
-  tables.forEach((value: string[], key: any, map) => {
-    const attrs: any[] = [];
-    value.forEach((attr: any) => {
-      attrs.push({
-        nodeId: key,
-        key: attr,
-        type: '',
-      });
-    });
-
-    let level = 'last';
-    const endIndex = key.lastIndexOf('-');
-    if (endIndex !== -1) {
-      const startIndex = key.lastIndexOf('_');
-      level = key.slice(startIndex + 1, endIndex);
-    }
-
-    const obj: any = {
-      id: key,
-      key: key,
-      label: key,
-      x: 100,
-      y: 100,
-      level: level,
-      attrs: attrs,
-    };
-    nodes.push(obj);
-  });
-};
-
-/**
- * 处理表级数据
- * @param data 表级数据
- * @returns 节点和边
- */
-export const collapseData = (data: any) => {
-  const nodes: any[] = [];
-  const edgeMap: Map<string, any> = new Map();
-  const tableFields: Set<any> = new Set();
-
-  data.forEach((item: any) => {
-    const targetFieldName = item.targetField.fieldName;
-    tableFields.add(targetFieldName);
-
-    if (!item.refFields) {
-      return;
-    }
-
-    createCollapsedEdge(edgeMap, tableFields, targetFieldName, item.refFields);
-  });
-
-  const edges = Array.from(edgeMap.values());
-  createCollapsedNode(nodes, tableFields);
-
-  console.log('collapseData', nodes, edges);
-  return {
-    nodes,
-    edges,
-  };
-};
-
-const createCollapsedEdge = (
-  edgeMap: Map<string, any>,
-  tableFields: Set<any>,
-  targetFieldName: string,
-  refFields: any[]
-) => {
-  const targetArray = targetFieldName.split('.');
-  refFields.forEach((ref: any) => {
-    const refFieldName = ref.fieldName;
-    tableFields.add(refFieldName);
-    const refArray = refFieldName.split('.');
-    if (targetArray[1] === refArray[1]) {
-      return;
-    }
-    const edge: any = {};
-    edge.source = refArray[1];
-    edge.sourceAnchor = refArray[1];
-    edge.target = targetArray[1];
-    edge.targetAnchor = targetArray[1];
-    edge.label = ref.label;
-    let key = refArray[1] + '-' + targetArray[1];
-    edgeMap.set(key, edge);
-  });
-};
-
-const createCollapsedNode = (nodes: any[], tableFields: Set<any>) => {
-  const tables: Set<string> = new Set();
-  tableFields.forEach((item: any) => {
-    const array = item.split('.');
-    let tableName = array[1];
-    tables.add(tableName);
-  });
-
-  tables.forEach((key: string, value: any) => {
-    const obj: any = {
-      id: key,
-      key: key,
-      label: key,
-      x: 100,
-      y: 100,
-      attrs: [],
-    };
-    nodes.push(obj);
-  });
-};
-
 export const dataTransform = (data: any) => {
   const nodes: any[] = [];
   const edges: any[] = [];
@@ -386,6 +177,238 @@ export const dataTransform = (data: any) => {
     nodes,
     edges,
   };
+};
+
+// 自定义数据转换
+export const transformData = (data: any) => {
+  const nodes: any[] = [];
+  // 用 map 实现对象去重
+  const edgeMap: Map<string, any> = new Map();
+  // 用 set 实现表名去重
+  const tableFields: Set<any> = new Set();
+
+  data.forEach((item: any) => {
+    const targetField = item.targetField;
+    const tableField = handleTableField(targetField);
+    tableFields.add(tableField);
+    if (item.refFields) {
+      createEdge(edgeMap, tableFields, tableField, item.refFields);
+    }
+  });
+
+  createNode(nodes, tableFields);
+  const edges = Array.from(edgeMap.values());
+
+  return {
+    nodes,
+    edges,
+  };
+};
+
+/**
+ * 创建 Edge 即连线即字段之间的连线
+ */
+const createEdge = (
+  edgeMap: Map<string, any>,
+  tableFields: Set<any>,
+  tableField: string,
+  refFields: any[]
+) => {
+  const target = getTableFieldName(tableField);
+  const targetName = target.tableName;
+  const targetAnchor = target.tableField;
+  refFields.forEach((ref: any) => {
+    const tableField = handleTableField(ref);
+    tableFields.add(tableField);
+
+    const source = getTableFieldName(tableField);
+    const sourceName = source.tableName;
+    const sourceAnchor = source.tableField;
+    // 不能自连，防止绘制失败
+    if (targetName === sourceName) {
+      return;
+    }
+
+    const edge: any = {};
+    edge.source = sourceName;
+    edge.sourceAnchor = sourceAnchor;
+    edge.target = targetName;
+    edge.targetAnchor = targetAnchor;
+    edge.label = ref.label;
+    let key = sourceName + sourceAnchor + '-' + targetName + targetAnchor;
+    edgeMap.set(key, edge);
+  });
+};
+
+/**
+ * 拼接表名+字段，逻辑可参考文档
+ */
+const handleTableField = (item: any) => {
+  const fieldName = item.fieldName;
+  let tableField = '';
+  if (item.final) {
+    tableField = fieldName;
+  } else {
+    tableField = `${item.level}-${item.index}:${fieldName}`;
+  }
+  return tableField;
+};
+
+/**
+ * 拆分字符串获取表名称，字段名称
+ */
+const getTableFieldName = (item: string) => {
+  const names: string[] = item.split(':');
+  let tableName = '';
+  let tableField = '';
+  if (names.length === 1) {
+    const array = names[0].split('.');
+    tableName = array[1];
+    tableField = array[2];
+  } else {
+    const array = names[1].split('.');
+    tableName = array[1] + '_' + names[0];
+    tableField = array[2];
+  }
+  return { tableName, tableField };
+};
+
+/**
+ * 获取表层级
+ */
+const getTableLevel = (tableField: string) => {
+  let level = 'last';
+  const endIndex = tableField.lastIndexOf('-');
+  if (endIndex !== -1) {
+    const startIndex = tableField.lastIndexOf('_');
+    level = tableField.slice(startIndex + 1, endIndex);
+  }
+  return level;
+};
+
+/**
+ * 创建 Node 即节点即表
+ */
+const createNode = (nodes: any[], tableFields: Set<any>) => {
+  const tables: Map<string, string[]> = new Map();
+  tableFields.forEach((item: any) => {
+    const table = getTableFieldName(item);
+    const tableName = table.tableName;
+    const tableField = table.tableField;
+
+    if (!tables.has(tableName)) {
+      tables.set(tableName, [tableField]);
+    } else {
+      const attrs: any = tables.get(tableName);
+      if (!attrs?.includes(tableField)) {
+        attrs?.push(tableField);
+        tables.set(tableName, attrs);
+      }
+    }
+  });
+
+  tables.forEach((value: string[], key: any, map) => {
+    const attrs: any[] = [];
+    value.forEach((attr: any) => {
+      attrs.push({
+        nodeId: key,
+        key: attr,
+        type: '',
+      });
+    });
+
+    const level = getTableLevel(key);
+    const obj: any = {
+      id: key,
+      key: key,
+      label: key,
+      x: 100,
+      y: 100,
+      level: level,
+      attrs: attrs,
+    };
+    nodes.push(obj);
+  });
+};
+
+/**
+ * 处理表级数据，即当字段级血缘关系为 false 时
+ */
+export const collapseData = (data: any) => {
+  const nodes: any[] = [];
+  const edgeMap: Map<string, any> = new Map();
+  const tableFields: Set<any> = new Set();
+
+  data.forEach((item: any) => {
+    const targetField = item.targetField;
+    const tableField = handleTableField(targetField);
+    tableFields.add(tableField);
+
+    if (item.refFields) {
+      createCollapsedEdge(edgeMap, tableFields, tableField, item.refFields);
+    }
+  });
+
+  const edges = Array.from(edgeMap.values());
+  createCollapsedNode(nodes, tableFields);
+
+  console.log('collapseData', nodes, edges);
+  return {
+    nodes,
+    edges,
+  };
+};
+
+const createCollapsedEdge = (
+  edgeMap: Map<string, any>,
+  tableFields: Set<any>,
+  tableField: string,
+  refFields: any[]
+) => {
+  const target = getTableFieldName(tableField);
+  const targetName = target.tableName;
+  refFields.forEach((ref: any) => {
+    const tableField = handleTableField(ref);
+    tableFields.add(tableField);
+    const source = getTableFieldName(tableField);
+    const sourceName = source.tableName;
+    // 不能自连，防止绘制失败
+    if (targetName === sourceName) {
+      return;
+    }
+
+    const edge: any = {};
+    edge.source = sourceName;
+    edge.sourceAnchor = sourceName;
+    edge.target = targetName;
+    edge.targetAnchor = targetName;
+    edge.label = ref.label;
+    let key = sourceName + '-' + targetName;
+    edgeMap.set(key, edge);
+  });
+};
+
+const createCollapsedNode = (nodes: any[], tableFields: Set<any>) => {
+  const tables: Set<string> = new Set();
+  tableFields.forEach((item: any) => {
+    const table = getTableFieldName(item);
+    const tableName = table.tableName;
+    tables.add(tableName);
+  });
+
+  tables.forEach((key: string, value: any) => {
+    const level = getTableLevel(key);
+    const obj: any = {
+      id: key,
+      key: key,
+      label: key,
+      x: 100,
+      y: 100,
+      level: level,
+      attrs: [],
+    };
+    nodes.push(obj);
+  });
 };
 
 /**
