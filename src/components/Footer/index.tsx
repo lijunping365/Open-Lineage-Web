f2982d984f3e05dc5bb0cf89da896a8e92f7aabe
lijunping <2544054976@qq.com>
2023-07-05 06:47:40 +0800

    :sparkles: 调整样式

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,19 +1,22 @@
-import logo from '/logo.svg';
 import metadata from '../../config/metadata';
 
-const Footer = () => {
+interface FooterProps {
+  nodeSize: number;
+  nodeLevel: number;
+}
+const Footer = ({ nodeSize, nodeLevel }: FooterProps) => {
   return (
     <>
-      <footer className='container body-font text-gray-600 absolute bottom-0 bg-white z-[200]'>
-        <div className='mx-auto flex flex-col items-center px-5 py-2 sm:flex-row'>
-          <span>节点：180</span>
-          <p className='mt-4 text-sm text-gray-500 sm:ml-4 sm:mt-0 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:pl-4'>
-            层数：10
+      <footer className='container body-font absolute bottom-0 bg-white z-[200]'>
+        <div className='mx-auto flex flex-col items-center px-5 py-2 sm:flex-row shadow-md'>
+          <p className='text-sm space-x-4 font-medium'>
+            <span>节点：{nodeSize}</span>
+            <span>层数：{nodeLevel}</span>
           </p>
-          <span className='mt-4 inline-flex justify-center sm:ml-auto sm:mt-0 sm:justify-start'>
+          <span className='inline-flex justify-center sm:ml-auto sm:mt-0 sm:justify-start'>
             <a
               href={metadata.website}
-              className='ml-1 text-gray-600'
+              className='ml-1 text-gray-600 text-sm'
               rel='noopener noreferrer'
               target='_blank'
             >
