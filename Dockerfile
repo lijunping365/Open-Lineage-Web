9b7405ae8f70b5ea581b0f742fb84d7fe2ff8466
code13 <code__13@outlook.com>
2023-10-13 20:27:06 +0800

    docker 部署

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# 安装Node.js
+FROM node:18-alpine
+
+# 创建工作目录
+WORKDIR /app
+
+# 将node_modules添加到工作目录
+COPY .npmrc package.json pnpm-lock.yaml ./
+RUN npm install -g pnpm@7.30.5 && pnpm i
+
+# 将所有文件复制到工作目录
+COPY . .
+
+# 编译前端项目
+RUN pnpm run build
+
+# 产生静态文件
+FROM nginx:stable-alpine
+
+COPY --from=0 /app/dist /usr/share/nginx/html/
+
+COPY nginx.conf /etc/nginx/conf.d/
---
+++ build.sh
@@ -0,0 +1,36 @@
+#源码路径
+SOURCE_PATH=/data/server/open-lineage/Open-Lineage-Web
+#docker 镜像/容器名字或服务名字 这里都命名为这个
+SERVER_NAME=open-lineage-web
+TAG=latest
+SERVER_PORT=10012
+#容器id
+CID=$(docker ps | grep "$SERVER_NAME" | awk '{print $1}')
+#镜像id
+IID=$(docker images | grep "$SERVER_NAME:$TAG" | awk '{print $3}')
+
+cd $SOURCE_PATH
+
+# 更新代码
+git pull
+
+# 容器存在则删除
+if [ -n "$CID" ]; then
+  echo "存在容器$SERVER_NAME, CID-$CID"
+  docker stop $SERVER_NAME
+  docker rm $SERVER_NAME
+fi
+
+# 构建docker镜像
+if [ -n "$IID" ]; then
+  echo "存在$SERVER_NAME:$TAG镜像，IID=$IID"
+  docker rmi $SERVER_NAME:$TAG
+else
+  echo "不存在$SERVER_NAME:$TAG镜像，开始构建镜像"
+  cd $SOURCE_PATH
+  docker build -t $SERVER_NAME:$TAG .
+fi
+
+# 运行docker容器
+docker run --name $SERVER_NAME -d -p $SERVER_PORT:80 $SERVER_NAME:$TAG
+echo "$SERVER_NAME容器创建完成"
